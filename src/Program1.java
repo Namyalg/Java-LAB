db93cabf296e937ac375eed299b33d8be331d9e3
Namya LG <53875297+Namyalg@users.noreply.github.com>
2021-04-10 15:30:40 +0530

    Update Program1.java

--- src/Program1.java
+++ src/Program1.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Program1 {
+import java.util.Scanner;
+
+public class Program1 {
+    public static void fun1(){
+        System.out.println("Im good");
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter seconds :  ");
+        int seconds = input.nextInt();
+        if(seconds < 0){
+            System.out.println("Time cannot be negative");
+            return;
+        }
+        else {
+            //System.out.println("Number of seconds " + seconds);
+            int hours = seconds / 3600;
+            seconds = seconds % 3600;
+            System.out.println(hours);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            System.out.print("Time in HH : MM : SS is ");
+            System.out.println(hours + " : " + minutes + " : " + seconds);
+            input.close();
+        }
+    }
 }
